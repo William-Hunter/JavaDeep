37633238bf3fab34f90a297785d1cad0fb759401
William-Hunter <a709209619@qq.com>
2017-01-08 20:10:50 +0800

    完成有向图的存储与遍历

--- src/datastructure/GraphTest.java
+++ src/datastructure/GraphTest.java
@@ -4,14 +4,31 @@ import java.util.Scanner;
 
 /**
  * Created by Administrator on 2017/1/8.
- * 有向图
+ * 目前只处理有向图
  */
 public class GraphTest {
 
     public static void main(String[] args) {
         Node[] vertexs=null;
         vertexs=createAdjacencyList(vertexs);
+        show(vertexs);
+        traversal(vertexs);
         System.out.println("=========END==========");
+    }
+
+    public static void traversal(Node[] vertexs){
+        System.out.println("display");
+        display(vertexs,vertexs[0]);
+    }
+
+    public static void display(Node[] vertexs,Node node){
+        String vec=getQueue(node);
+        while(vec!=null){
+            int vector=Integer.valueOf(vec);
+            System.out.println(node.value+"----->"+vertexs[vector].value);
+            display(vertexs,vertexs[vector]);
+            vec=getQueue(node);
+        }
     }
 
     public static Node[] createAdjacencyList(Node[] vertexs) {
@@ -26,27 +43,39 @@ public class GraphTest {
         }
         for (int index = 0; index < vertexs.length; index++) {
             System.out.println("请输入节点" + vertexs[index].value + "的去向，请以英文逗号分隔");
-            String[] context = sc.nextLine().split(",");
+            String line=sc.nextLine();
+            if (line.isEmpty()){
+                break;
+            }
+            String[] context = line.split(",");
             for (int count = 0; count < context.length; count++) {         //循环输入的节点
-                String value="";
+                String value=null;
                 for(int order=0;order<vertexs.length;order++){
                     if (vertexs[order].value.equals(context[count])) {
-                        value=order+"";
+                        value=String.valueOf(order);
                         break;
                     }
                 }
-                vertexs[index].next=LinkedList.add(vertexs[index].next, 1, value + "");
+                vertexs[index].next=LinkedList.add(vertexs[index].next, 1, value);
             }
-            LinkedList.list(vertexs[index].next);
         }
-        System.out.println("创建完毕");
         return vertexs;
     }
 
+    public static void show(Node[] vertexs){
+        for(int index=0;index<vertexs.length;index++){
+            System.out.print(vertexs[index].value+":");
+            LinkedList.list(vertexs[index].next);
+            System.out.println();
+        }
+    }
 
-}
-
-class Vertex {
-
-
+    public static String getQueue(Node head){
+        String retu=LinkedList.get(head,2);
+        if(retu!=null&&LinkedList.delete(head,2)){
+            return retu;
+        }else{
+            return null;
+        }
+    }
 }
--- src/datastructure/LinkedList.java
+++ src/datastructure/LinkedList.java
@@ -10,7 +10,6 @@ public class LinkedList {
         Scanner sc = new Scanner(System.in);
         Node head = new Node();
         head.value = "shit";
-
         boolean retu = false;
         while (!retu) {
             String word = "1:显示list\n2:增加一个元素\n3:删除一个元素\n4:修改一个元素\n5:退出\n";
@@ -49,9 +48,12 @@ public class LinkedList {
         System.out.println("Bye Bye");
     }
 
+    /**
+     * index从1开始
+     */
     public static Node add(Node head, int index, String value) {
         Node current = head;
-        for (int count = 0; count < index - 2; count++) {
+        for (int count = 1; count < index - 1; count++) {
             current = current.next;
         }
         Node ne = new Node();
@@ -66,21 +68,59 @@ public class LinkedList {
         return head;
     }
 
-    public static Node delete(Node head, int index) {
-        for (int count = 0; count < index - 1; count++) {
-
+    /**
+     * index从1开始
+     */
+    public static boolean delete(Node head, int index) {
+        try {
+            Node previous = null;
+            Node current = head;
+            for (int count = 1; count < index; count++) {
+                previous = current;
+                current = current.next;
+            }
+            if (index == 1) {
+                head = current.next;
+            } else {
+                previous.next = current.next;
+            }
+            return true;
+        } catch (NullPointerException e) {
+//            e.printStackTrace();
+            return false;
         }
-
-        return head;
     }
 
-    public static void update(Node node, int index, String value) {
+    public static boolean update(Node head, int index, String value) {
+        try {
+            Node current = head;
+            for (int count = 1; count < index; count++) {
+                current = current.next;
+            }
+            current.value = value;
+            return true;
+        } catch (NullPointerException e) {
+//            e.printStackTrace();
+            return false;
+        }
+    }
 
+    public static String get(Node head, int index) {
+        try {
+            Node current = head;
+            for (int count = 1; count < index; count++) {
+                current = current.next;
+            }
+            return current.value;
+        } catch (NullPointerException e) {
+//            e.printStackTrace();
+            return null;
+        }
     }
 
     public static void list(Node node) {
         if (node != null) {
-            System.out.println(node.value + "\t");
+            System.out.print(node.value + "\t");
             list(node.next);
         }
     }

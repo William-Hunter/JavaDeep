08bbc6617114b6518bbaefcdf50ec580a098ab3f
William-Hunter <a709209619@qq.com>
2017-02-12 20:36:03 +0800

    use and test the junit lib.

---
+++ src/junit/Calc.java
@@ -0,0 +1,24 @@
+package junit;
+
+/**
+ * Created by Administrator on 2017/2/12.
+ */
+
+public class Calc{
+
+    public int add(int a,int b){
+        return a+b;
+    }
+
+    public int lose(int a,int b){
+        return a-b;
+    }
+
+    public int div(int a,int b){
+        return a/b;
+    }
+
+    public int mar(int a,int b){
+        return a*b;
+    }
+}
